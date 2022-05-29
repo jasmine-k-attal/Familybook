110687a9918f20f280d5e17ee4ce5f0cb7b03a62
jasmine-k-attal <98331461+jasmine-k-attal@users.noreply.github.com>
2022-05-29 12:51:50 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+            var images = ["father.JPG","mother.JPG","sibling.JPG","me.JPG"];
+            var names = ["Dad", "Mom", "Sibling", "Me (Jasmine Kaur)"];
+            var i = 0;
+            function update()
+            {
+                i++;
+                var numbers_of_family_member_in_array = 4
+                if(i > numbers_of_family_member_in_array )
+                {
+                    i = 0;
+                }
+                var updatedImage = images[i];
+                var updatedName  = names[i];
+                document.getElementById("family_member_image").src = updatedImage;
+                document.getElementById("family_member_name").innerHTML = updatedName;
+            }
